d50c9dff4844db6205838bc74780f1e10e221f97
arveyk <harveykisiangani@gmail.com>
2024-08-12 10:33:09 +0300

    Eslint

--- utils/redis.js
+++ utils/redis.js
@@ -1,13 +1,14 @@
-import { createClient } from 'redis';
+// import { createClient } from 'redis';
+import * as redis from 'redis';
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
+    this.client = redis.createClient();
     this.client.on('error', (error) => console.log(error));
     this.isOpen = false;
     (() => {
       try {
-	this.isOpen = true;
+        this.isOpen = true;
         this.client.connect()
       } catch (error) {
           console.error('Connection error');
@@ -20,20 +21,33 @@ class RedisClient {
   }
 
   async get(key) {
-    return await this.client.get(key);
+    if (this.client.isOpen === true) {
+      const value = await this.client.get(key);
+      return value;
+    }
   }
+
   async set(key, value, duration) {
-    await this.client.set(key, value, { EX: duration });
-  }
-  async del(key) {
-    this.client.del(key, function(err, reply) {
+    await this.client.set(key, value, { EX: duration }, (err, reply) => {
       if (err) {
-        throw err;
+	return null;
       }
-      console.log(reply);
     });
   }
-}
 
+  async del(key) {
+    try {
+      await this.client.del(key, function(err, response) {
+        if (response == 1) {
+          console.log(response);
+        } else {
+          console.log('error');
+        }
+      }); 
+    } catch (error) {
+	 return null;
+    }
+  }
+}
 const redisClient = new RedisClient();
 module.exports = redisClient;
